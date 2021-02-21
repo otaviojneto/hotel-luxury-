b5e53618f763224d4aede2612eb11071a635e66c
Neto Lourenco <otavojnetoo@gmail.com>
2021-02-21 00:28:20 -0300

    acrescentado as rotas

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      fghjk
+    </div>
+  )
+}
+
+export default Contact;
---
+++ src/pages/Facilities/Facilities.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Facilities = () => {
+  return (
+    <div>
+      fghjk
+    </div>
+  )
+}
+
+export default Facilities;
